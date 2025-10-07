a297b742c92a825608a494f3b96b9b307ca624bb
Liviu <silion.liviu@yahoo.com>
2025-10-08 01:50:09 +0200

    oops

--- web/src/common/components/info-tooltip.tsx
+++ web/src/common/components/info-tooltip.tsx
@@ -1,5 +1,4 @@
-import {useRef, useState} from 'react';
-import type {FocusEvent} from 'react';
+import {useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import infoIcon from '../../assets/info.png';
 import '../styles/tooltip.css';
